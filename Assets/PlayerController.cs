680156cb1b97790815445213406660e57932c6d4
Carter Hinsley <kvazau@protonmail.com>
2019-03-02 19:50:16 -0500

    Add player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform leftBoundary;
+    public Transform rightBoundary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLocation();
+    }
+
+    void UpdateLocation()
+    {
+        transform.Translate(Vector3.right *
+                            moveSpeed *
+                            Time.deltaTime *
+                            Input.GetAxisRaw("Horizontal"));
+        
+        if (transform.position.x < leftBoundary.position.x)
+        {
+            UpdateX(leftBoundary.position.x);
+        }
+        else if (transform.position.x > rightBoundary.position.x)
+        {
+            UpdateX(rightBoundary.position.x);
+        }
+    }
+
+    void UpdateX(float newX)
+    {
+        transform.position = new Vector3(
+            newX,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
