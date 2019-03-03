912063beca0bdb212b51ba49294e3f210df43b52
Carter Hinsley <kvazau@protonmail.com>
2019-03-03 13:51:26 -0500

    Make enemies fire

---
+++ Assets/Scripts/EnemyBulletSpawnPointController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletSpawnPointController : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(
+            player.position.x,
+            player.position.y,
+            transform.position.z
+        );
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        float z = Vector3.SignedAngle(Vector3.up, direction, Vector3.forward);
+        transform.rotation = Quaternion.Euler(0, 0, z);
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,17 +6,34 @@ public class EnemyController : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
+    // How many bullets are fired per second.
+    public float fireRate;
+    public bool firing = false;
+
+    private float firingTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("GenerateBullet", 0, 1);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (firing)
+        {
+            firingTimer += Time.deltaTime;
+            if (firingTimer >= 1 / fireRate)
+            {
+                GenerateBullet();
+                ResetTimer();
+            }
+        }
+        else
+        {
+            ResetTimer();
+        }
     }
 
     void GenerateBullet()
@@ -24,5 +41,10 @@ public class EnemyController : MonoBehaviour
         Instantiate(bulletPrefab,
                     bulletSpawnPoint.position,
                     bulletSpawnPoint.rotation);
+    }
+
+    void ResetTimer()
+    {
+        firingTimer = 0;
     }
 }
