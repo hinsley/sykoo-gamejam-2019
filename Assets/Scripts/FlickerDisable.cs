94cc149b98a64035ff14837fee5996e29fe8e494
Carter Hinsley <kvazau@protonmail.com>
2019-03-04 00:26:14 -0500

    Add flickerdisable script

---
+++ Assets/Scripts/FlickerDisable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerDisable : MonoBehaviour
+{
+    [Tooltip("Flicker rate per second.")]
+    public float flickerRate;
+    public GameObject flickeringGameObject;
+
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= 1 / flickerRate)
+        {
+            flickeringGameObject.SetActive(flickeringGameObject.activeSelf);
+        }
+        timeElapsed = timeElapsed % (1 / flickerRate);
+    }
+}
