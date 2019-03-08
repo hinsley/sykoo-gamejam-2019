781c7704c3007922e9340506688c0c9578c0a4e8
Carter Hinsley <kvazau@protonmail.com>
2019-03-08 00:57:10 -0500

    Add music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip musicClip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource audioSource = Utils.GetAudioSource();
+        if (audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(musicClip);
+        }
+    }
+}
