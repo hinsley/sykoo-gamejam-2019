7bd29feedec0a3d86a374b3716913ea68b76ebf7
Phillip Hinsley <philliphinsley@gmail.com>
2019-03-08 14:41:02 -0500

    Changed Logo and Splash screen settings with new icon and screenshots.

---
+++ Assets/DisableTextAfterSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableTextAfterSeconds : MonoBehaviour
+{
+public Text Text1;
+public int timer;
+   void Start () 
+   {
+      Invoke("DisableText", timer);
+   }
+   void DisableText()
+   { 
+      Text1.enabled = false; 
+   }    
+}
