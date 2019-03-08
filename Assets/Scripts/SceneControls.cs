5becfd86044b28696de44bf24f23bb8e96a0e49e
Phillip Hinsley <philliphinsley@gmail.com>
2019-03-08 13:14:26 -0500

    Created Start Screen and transition to Main Scene.

---
+++ Assets/Scripts/SceneControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControls : MonoBehaviour
+{
+
+    public bool cursorLocked = true;
+
+    void Update() 
+    {
+
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main Scene");
+        }
+        
+        if (cursorLocked == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (Input.GetKeyDown (KeyCode.F9))
+            {
+                cursorLocked = false;
+            }
+        }
+        else if (cursorLocked == false)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            if (Input.GetKeyDown (KeyCode.F9))
+            {
+                cursorLocked = true;
+            }
+        }
+
+
+        
+        // SceneManager.LoadScene("Main Scene");
+        // Debug.Log ("Text: ");
+    } 
+    
+}
